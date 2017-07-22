bdc3b80de0a170fdc1b6c5944cad8f5aaa435f65
JeromeJonathan7 <JeromeJonathan7@users.noreply.github.com>
2017-07-22 17:04:38 +0530

    Update createcommandwords.py
    1.Updated the command words
    2.I think TD_RT10 command word is wrong.It is given 5450 but it is 5440.So therefore changed it here and in app1.py(dependency file).

--- createcommandwords.py
+++ createcommandwords.py
@@ -1,7 +1,7 @@
 import os
 
 def createcommandwords():
-    l=("4646","4647","4648","4649","4650","4651")
+    l=("F820","F840","F860","F880","F8A0","F8C0","3C20","4420","4C20","5420","38E5","40E5","48E5","50E5","3C40","4440","4C40","5440","3C65","4465","4C65","5465","5204")
     file=open(os.getcwd()+"\\commandwords.txt","w+")
     for word in l:
         file.write(word+"\n")
