4d09906e0d0a489feb7a02f142a93b0e2a406d20
JeromeJonathan7 <JeromeJonathan7@users.noreply.github.com>
2017-07-22 22:16:37 +0530

    Add files via upload
    1.These are app1 files
    2.NO NEED OF UPLOADING a.)app1repname.txt b.)commandwords.txt AS THEY ARE AUTOMATICALLY GENERATED
    3.The non-uploaded files are generated in the home.py destination directory

---
+++ app1/datalist.py
@@ -0,0 +1,7 @@
+def convert(commandword):
+    arr = []
+    inp = open("data1/" + str(commandword) + ".txt", "r+")  # read line into array
+    for line in inp.readlines():  # loop over the elemets, split by whitespace
+        for i in line.split():
+            arr.append(i)  # convert to integer and append to the list
+    return arr
